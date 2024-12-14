32ac341dd09a95c2ae6d5bdce9eb87335c449994
QobilToshmurodov <qobiltoshmurodov289@gmail.com>
2024-12-14 09:26:02 +0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Enum.Program;
+
+namespace Enum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Importance qiymatini kiriting (None, Trivial, Regular, Important, Critical):");
+            string input = Console.ReadLine();
+            Importance result = (Importance)System.Enum.Parse(typeof(Importance), "Critical");
+            if (result == Importance.Critical)
+            {
+                Console.WriteLine("O'ta muhim!");
+            }
+            else
+            {
+                Console.WriteLine($"Kiritilgan qiymat: {result}");
+            }
+        }
+        public enum Importance
+        {
+
+            None,
+            Trivial,
+            Regular,
+            Important,
+            Critical
+        }
+    }
+}
